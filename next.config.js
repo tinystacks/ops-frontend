8cf591e95d19f77f794f6ec4994b9b937b4252b9
Caleb Courier <caleb.courier@gmail.com>
2023-03-30 14:21:26 -0500

    remove webpack resolvers

--- next.config.js
+++ next.config.js
@@ -3,43 +3,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    const {
-      resolve = {},
-      resolveLoader = {}
-    } = config || {};
-    const {
-      modules: resolveModules = []
-    } = resolve;
-    const { modules: resolveLoaderModules = [] } = resolveLoader;
-    
-    const dependenciesPath = path.resolve(__dirname, '../dependencies/node_modules/')
-
-    resolveModules.push(dependenciesPath);
-    resolve.modules = resolveModules;
-    
-    resolveLoaderModules.push(dependenciesPath);
-    resolveLoader.modules = resolveLoaderModules;
-
-    const newConfig = {
-      ...config,
-      resolve
-    };
-
-    // fs.writeFileSync('./next-webpack-config.js', JSON.stringify(
-    //   newConfig,
-    //   (key, value) => {
-    //     return typeof value === "bigint" ? value.toString() + "n" : value;
-    //   },
-    //   2
-    // ));
-
-    return newConfig;
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
