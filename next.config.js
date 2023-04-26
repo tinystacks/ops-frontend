137610172bcde33bebca7101c9934d6dd7bf8cf4
Akash Kedia <kedia23akash@gmail.com>
2023-04-26 11:45:45 -0700

    revert sso fix

--- next.config.js
+++ next.config.js
@@ -3,18 +3,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        child_process: false
-      };
-    }
-
-    return config;
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
