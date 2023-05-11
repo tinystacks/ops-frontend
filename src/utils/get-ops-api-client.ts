808d9138130b4fbfcb36bc8d5004e973df78cdad
Caleb Courier <caleb.courier@gmail.com>
2023-05-11 09:19:34 -0500

    trim trailing slash on urls, break after 3 errors

--- src/utils/get-ops-api-client.ts
+++ src/utils/get-ops-api-client.ts
@@ -1,8 +1,18 @@
 import { OpenAPIConfig, OpsApiClient } from '@tinystacks/ops-model';
 import { fetchApiKey } from 'ops-frontend/utils/fetch-api-key';
 
+
+function trimTrailingSlash (url: string): string {
+  if (url?.endsWith('/')) {
+    const trimmedUrl = url.substring(0, url.length - 1);
+    return trimTrailingSlash(trimmedUrl);
+  }
+  return url;
+}
+
 export async function getOpsApiClient () {
-  const baseEndpoint = process.env.API_ENDPOINT;
+  const apiEndpoint = process.env.API_ENDPOINT;
+  const baseEndpoint = trimTrailingSlash(apiEndpoint!);
     const apiSecret = await fetchApiKey();
     const clientOptions: Partial<OpenAPIConfig> = {
       BASE: baseEndpoint
