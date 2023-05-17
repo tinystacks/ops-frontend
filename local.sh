69756f88b8ce0772f6ecadf0bb0af12e3d380b21
Akash Kedia <kedia23akash@gmail.com>
2023-05-16 17:45:51 -0700

    removed echo's

--- local.sh
+++ local.sh
@@ -2,10 +2,8 @@
 
 ## Install runtime dependencies
 dependencies=$(bash ./get-runtime-dependencies.sh);
-echo $dependencies;
 npm i --no-save --silent $dependencies --@tinystacks:registry=https://registry.npmjs.org;
 dependencyNames=$(bash ./get-runtime-dependencies.sh -n);
-echo $dependencyNames;
 node ./generate-plugins-index.js $dependencyNames;
 
 ## Setup environment variables
